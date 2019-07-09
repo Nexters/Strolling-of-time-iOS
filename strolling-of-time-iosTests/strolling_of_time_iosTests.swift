57f9ea1954d4488878364b63a370d4989458b307
Mincheol Kim <mcauto@naver.com>
2019-07-09 19:25:05 +0900

    Initial Commit

---
+++ strolling-of-time-iosTests/strolling_of_time_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  strolling_of_time_iosTests.swift
+//  strolling-of-time-iosTests
+//
+//  Created by mcauto on 09/07/2019.
+//  Copyright © 2019 wiw. All rights reserved.
+//
+
+import XCTest
+@testable import strolling_of_time_ios
+
+class strolling_of_time_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
